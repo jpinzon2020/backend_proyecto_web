e9e23b3001603edc50cfb231dcb2b782c0ace53e
jpinzontecno <jorge.pinzon@tecno.co>
2022-04-09 13:46:44 -0500

    Se crear clases e interfaces para enviar Email.

---
+++ src/main/java/com/poli/project/controller/MailController.java
@@ -0,0 +1,32 @@
+package com.poli.project.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.poli.project.model.Mail;
+import com.poli.project.service.MailService;
+
+
+@RestController
+public class MailController {
+
+	@Autowired
+	private MailService notificationService;
+	
+	/* JSON email...
+	{
+	    "mailTo": "wsoto@poligran.edu.co",
+	    "mailSubject": "hola mundo",
+	    "mailContent": "bienvenidos",
+	    "attachments" : ["/Users/wilsonsoto/Downloads/TransaccionPNCBANK.pdf"]
+	}
+	*/
+	@RequestMapping(value = "/api/v1/msg", method=RequestMethod.GET)
+	public String sendEmail(@RequestBody Mail mail) {
+		notificationService.sendEmail(mail);
+		return "Email sent successfully";
+	}
+}
---
+++ src/main/java/com/poli/project/service/MailService.java
@@ -0,0 +1,9 @@
+package com.poli.project.service;
+
+import com.poli.project.model.Mail;
+
+public interface MailService {
+
+	public void sendEmail (Mail mail);
+	
+}
