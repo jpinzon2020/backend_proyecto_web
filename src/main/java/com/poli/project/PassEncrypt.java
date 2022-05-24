591257c14b36c50cfa8515366aa79fbdf09cf413
jpinzontecno <jorge.pinzon@tecno.co>
2022-05-24 15:33:45 -0500

    Crear entrada por consola para cifrar un string.

--- src/main/java/com/poli/project/PassEncrypt.java
+++ src/main/java/com/poli/project/PassEncrypt.java
@@ -1,12 +1,16 @@
 package com.poli.project;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import java.util.Scanner;
 
 public class PassEncrypt {
 
 	public static void main(String[] args) {
-
-		String password = "123456";
+		
+		Scanner read = new Scanner(System.in);
+		
+		System.out.println("Ingrese su constraseña a cifrar");
+		String password = read.next();
 		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		String hashedPassword = passwordEncoder.encode(password);
 
